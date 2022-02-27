3c8985668d63070aff3fb60764ad349bf99da437
tomoki737 <tomoki.k2159@gmail.com>
2022-02-27 14:47:59 +0900

    テストの追加

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -48,7 +48,7 @@
                 @endif
 
                 <x-button class="ml-3">
-                    {{ __('Log in') }}
+                    ログイン
                 </x-button>
             </div>
         </form>
--- tests/Feature/Auth/EmailVerificationTest.php
+++ tests/Feature/Auth/EmailVerificationTest.php
@@ -16,7 +16,7 @@ class EmailVerificationTest extends TestCase
 
     public function test_email_verification_screen_can_be_rendered()
     {
-        $user = User::factory()->create([
+        $user = factory(User::class)->create([
             'email_verified_at' => null,
         ]);
 
@@ -27,7 +27,7 @@ class EmailVerificationTest extends TestCase
 
     public function test_email_can_be_verified()
     {
-        $user = User::factory()->create([
+        $user = factory(User::class)->create([
             'email_verified_at' => null,
         ]);
 
@@ -48,7 +48,7 @@ class EmailVerificationTest extends TestCase
 
     public function test_email_is_not_verified_with_invalid_hash()
     {
-        $user = User::factory()->create([
+        $user = factory(User::class)->create([
             'email_verified_at' => null,
         ]);
 
