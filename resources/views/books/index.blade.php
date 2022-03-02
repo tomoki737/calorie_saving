be064cf010ca4af09784c6d192decbec64096b83
tomoki737 <tomoki.k2159@gmail.com>
2022-03-02 16:31:08 +0900

    検索画面の作成

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -1,43 +1,37 @@
-<!DOCTYPE html>
-<html lang="ja">
-    <head>
-        <meta charset="UTF-8">
-        <title>PHP/LaravelでGoogle books apiを使うサンプル</title>
-    </head>
-    <body>
-        <h1>PHP/LaravelでGoogle books apiを使うサンプル</h1>
-        <form action="/" method="get">
-            書籍名:<input type="text" name="keyword" size="50" value="{{ $keyword }}">&nbsp;<input type="submit" value="検索">
-        </form>
+@extends("layouts/app")
+@section('title', '本を検索')
+@section('content')
+@include('layouts/nav')
+<div class="container" style="max-width: 1000px;">
+    <div class="row">
+        <h1 class="mt-2">本を検索</h3>
+            <body>
+                <form action="{{ route('books.index') }}" method="get">
+                    <div class="col-sm-5">
+                        <!-- <input type="text" placeholder="書籍名" class="form-control border-bottom border-2 p-0 border-dark" name="keyword" value="{{ $keyword }}" style="border:none; border-color: inherit; box-shadow: none; border-radius: 0px;"> -->
+                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="書籍名"  name="keyword" value="{{ $keyword }}">
+</div>
+                    </div>
+                </form>
 
-        @if ($items == null)
-            <p>書籍名を入力してください。</p>
-        @else (count($items) > 0)
-            <p>「{{ $keyword }}」の検索結果</p>
-            <hr>
-            @foreach ($items as $item)
-            <h2>{{ $item['volumeInfo']['title']}}</h2>
-                @if (array_key_exists('imageLinks', $item['volumeInfo']))
-                    <img src="{{ $item['volumeInfo']['imageLinks']['thumbnail']}}"><br>
-                @endif
-
-                @if (array_key_exists('description', $item['volumeInfo']))
-                    著者：{{ $item['volumeInfo']['authors'][0]}}<br>
-                @endif
-                @if (array_key_exists('description', $item['volumeInfo']))
-                    発売年月：{{ $item['volumeInfo']['publishedDate']}}<br>
-                @endif
-                <br>
-                @foreach ($item['volumeInfo']['industryIdentifiers'] as $industryIdentifier)
-                    {{ $industryIdentifier['type'] }}&nbsp;：&nbsp;{{ $industryIdentifier['identifier'] }} <br>
-                @endforeach
-                <br>
-                @if (array_key_exists('description', $item['volumeInfo']))
-                    概要:{{ $item['volumeInfo']['description']}}<br>
-                @endif
-                <br>
+                @if ($items == null)
+                <p>書籍名を入力してください。</p>
+                @else (count($items) > 0)
+                <p>「{{ $keyword }}」の検索結果</p>
                 <hr>
-            @endforeach
-        @endif
-    </body>
-</html>
+                @foreach ($items as $item)
+                <h2>{{ $item['volumeInfo']['title']}}</h2>
+                @if (array_key_exists('imageLinks', $item['volumeInfo']))
+                <img src="{{ $item['volumeInfo']['imageLinks']['thumbnail']}}" style="max-width: 200px"><br>
+                @endif
+                @if (array_key_exists('description', $item['volumeInfo']))
+                <p>発売年月:{{ $item['volumeInfo']['publishedDate']}}</p>
+                @endif
+                @if (array_key_exists('description', $item['volumeInfo']))
+                <p>概要:{{ $item['volumeInfo']['description']}}</p>
+                @endif
+                <hr>
+                @endforeach
+                @endif
+    </div>
+</div>
