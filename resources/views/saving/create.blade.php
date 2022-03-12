fac35d8b9a5f0d4007bcb3931bebdf71d474be12
tomoki737 <tomoki.k2159@gmail.com>
2022-03-12 15:37:07 +0900

    目標画面の編集

--- resources/views/saving/create.blade.php
+++ resources/views/saving/create.blade.php
@@ -5,8 +5,16 @@
 <div class="container" style="max-width: 1000px;">
     <div class="row">
         @include('layouts/error')
-        <h3 class="mt-2">目標を作成</h3>
-        
+        <form action="route('savings.store')">
+            <h3 class="mt-2">目標を作成</h3>
+            <p class="mt-5">目標の内容</p>
+            <input type="text" class="form-control" name="goal">
+            <p class="mt-3">目標カロリー</p>
+            <input type="text" class="form-control" name="calorie">
+            <div class="text-end mt-3">
+                <button class="btn btn-primary col-3 pt-3 pe-2 ps-2  pb-3 fs-5" type="submit">作成する</button>
+            </div>
+        </form>
     </div>
 </div>
 @endsection
